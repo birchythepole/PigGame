e445aefae6e49cfcf79733173e15bcd5ea65b3e7
BirchyThePole <birchythepole@gmail.com>
2018-12-21 14:14:52 +0100

    Commit 4: Added working Hold button, and winner options

--- app.js
+++ app.js
@@ -33,22 +33,36 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
         //Add score
         roundScore += dice;
         document.querySelector('#current-' + activePlayer).textContent = roundScore;
-    }
-    else {
+    } else {
         //Next Player
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-        roundScore = 0;
-        document.getElementById('current-0').textContent = '0';
-        document.getElementById('current-1').textContent = '0';
-        document.querySelector('.player-0-panel').classList.toggle('active');
-        document.querySelector('.player-1-panel').classList.toggle('active');
-        //document.querySelector('palyer-0-panel').classList.remove = ('active');
-        //document.querySelector('palyer-1-panel').classList.add = ('active');
-        document.querySelector('.dice').style.display = 'none';
+        nextPlayer();
     }
 });
 document.querySelector('.btn-hold').addEventListener('click', function () {
     //Add CURRENT score to GLOBAL score
+    scores[activePlayer] += roundScore;
     // Update the UI
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
     // Check if layer won the game
+    if (scores[activePlayer] >= 100) {
+        document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+    } else {
+        //Next Player
+        nextPlayer();
+    };
 });
+
+function nextPlayer() {
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+    //document.querySelector('palyer-0-panel').classList.remove = ('active');
+    //document.querySelector('palyer-1-panel').classList.add = ('active');
+    document.querySelector('.dice').style.display = 'none';
+};
