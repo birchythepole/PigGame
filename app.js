65b9492c8ac20a46d4a06d4fe5142d5cbc2a9286
BirchyThePole <birchythepole@gmail.com>
2018-12-21 14:34:03 +0100

    Commit 5 added working init function and paried it with NewGame button

--- app.js
+++ app.js
@@ -9,18 +9,7 @@ GAME RULES:
 
 */
 var scores, roundScore, activePlayer;
-scores = [0, 0];
-roundScore = 0;
-activePlayer = 0;
-//document.querySelector('#current-' + activePlayer).textContent = dice;
-//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
-//var x = document.querySelector('#curent-0');
-//console.log(x);
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
-document.querySelector('.dice').style.display = 'none';
+init();
 document.querySelector('.btn-roll').addEventListener('click', function () {
     //1.Random Number
     var dice = Math.floor(Math.random() * 6) + 1;
@@ -54,6 +43,7 @@ document.querySelector('.btn-hold').addEventListener('click', function () {
         nextPlayer();
     };
 });
+document.querySelector('.btn-new').addEventListener('click', init);
 
 function nextPlayer() {
     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
@@ -65,4 +55,26 @@ function nextPlayer() {
     //document.querySelector('palyer-0-panel').classList.remove = ('active');
     //document.querySelector('palyer-1-panel').classList.add = ('active');
     document.querySelector('.dice').style.display = 'none';
+};
+
+function init() {
+    scores = [0, 0];
+    roundScore = 0;
+    activePlayer = 0;
+    //document.querySelector('#current-' + activePlayer).textContent = dice;
+    //document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+    //var x = document.querySelector('#curent-0');
+    //console.log(x);
+    document.querySelector('.dice').style.display = 'none';
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
 };
